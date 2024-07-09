df6bf3ed4ac56b36a0f617d2e79d3029da8046c4
Nzimnevich <n>
2024-07-09 20:48:20 +0300

    Задание №2: Создать первый работающий тест

---
+++ csharp_training.Tests/UnitTest1Test.cs
@@ -0,0 +1,17 @@
+using csharp_training;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csharp_training.Tests;
+
+[TestClass]
+
+public class UnitTest1Test
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
--- csharp_training/adressbook_web_test.cs
+++ csharp_training/adressbook_web_test.cs
@@ -2,5 +2,13 @@ namespace csharp_training;
 
 public class adressbook_web_test
 {
-    
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1
+        {
+            
+        }
+    }
 }
